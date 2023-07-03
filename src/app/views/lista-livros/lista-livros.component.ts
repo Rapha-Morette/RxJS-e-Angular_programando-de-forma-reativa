fcd34d4f39085a58e29c46abc8a04db5d6a1e691
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-07-03 14:46:05 -0300

    Módulo 1 video 3 concluído

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
@@ -8,9 +9,12 @@ import { Component } from '@angular/core';
 export class ListaLivrosComponent {
 
   listaLivros: [];
+  campoBusca: string = ''
+  constructor(private service: LivroService) { }
 
-  constructor() { }
-
+  buscarLivros() {
+    this.service.buscar(this.campoBusca);
+  }
 }
 
 
